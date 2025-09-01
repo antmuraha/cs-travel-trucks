322e412903e7341c52fc0cac4976c1fb2b0df67f
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-09-01 22:17:54 +0300

    Add  Proxy Deployment Guide and Vite configuration file
    - Created a new README file for the Proxy deployment on Google Cloud Functions, including setup instructions, prerequisites, deployment steps, and local development instructions.
    - Added a Vite configuration file to set up a React project with Vite.

---
+++ proxy/index.js
@@ -0,0 +1,36 @@
+const SECRET_SLUG = process.env.SECRET_SLUG;
+
+export const proxy = async (req, res) => {
+  res.set(
+    'Access-Control-Allow-Origin',
+    process.env.NODE_ENV === 'production' ? 'https://cs-travel-trucks.vercel.app' : '*'
+  );
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  res.set('Accept-Version', 'v1');
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).send('');
+    return;
+  }
+
+  try {
+    const url = req.url;
+    const method = req.method;
+
+    const urlApi = `https://${SECRET_SLUG}.mockapi.io${url}`;
+
+    console.log(`Fetching from: ${urlApi}`);
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const response = await fetch(urlApi, { method, headers });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching from IMDB');
+  }
+};
