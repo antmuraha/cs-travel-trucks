317a79e83dcdca58519b18ad5c81f7d647d190de
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-09-06 06:49:58 +0300

    Refactor Header component: update layout and styles; enhance Hero button functionality with navigation

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,29 +1,40 @@
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
+import routes from '@constants/routes';
 import { Logo } from '@controls/Logo';
 
 import styles from './index.module.css';
-import routes from '@constants/routes';
 
 export const Header = () => {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
   return (
-    <header className={styles.header}>
-      <NavLink className={styles.headerLogo} to={routes.home()} aria-label="Logo">
-        <Logo />
-      </NavLink>
-      <nav>
-        <ul>
-          <li className={styles.headerNavItemHome}>
-            <NavLink to={routes.home()} className={({ isActive }) => (isActive ? styles.active : '')}>
-              Home
-            </NavLink>
-          </li>
-          <li className={styles.headerNavItemCatalog}>
-            <NavLink to={routes.catalog()} end className={({ isActive }) => (isActive ? styles.active : '')}>
-              Catalog
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className={`${styles.header} ${isHome ? styles.headerHome : ''}`}>
+      <div className={styles.headerContent}>
+        <NavLink
+          className={`${styles.headerLogo} ${isHome ? styles.headerLogoHome : ''}`}
+          to={routes.home()}
+          aria-label="Logo"
+          style={{ viewTransitionName: 'header-logo' }}
+        >
+          <Logo />
+        </NavLink>
+        <nav>
+          <ul>
+            <li className={styles.headerNavItemHome}>
+              <NavLink to={routes.home()} className={({ isActive }) => (isActive ? styles.active : '')}>
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.headerNavItemCatalog}>
+              <NavLink to={routes.catalog()} end className={({ isActive }) => (isActive ? styles.active : '')}>
+                Catalog
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div />
+      </div>
       <div />
     </header>
   );
--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -1,13 +1,23 @@
+import { useNavigate } from 'react-router';
+import routes from '@constants/routes';
+import useViewTransition from '@hooks/useViewTransition';
 import { Button } from '../../controls/Button';
 
 import styles from './index.module.css';
 
 export const Hero = () => {
+  const navigate = useNavigate();
+  const startTransition = useViewTransition(() => {
+    navigate(routes.catalog());
+  });
+
   return (
     <div className={styles.hero}>
       <h1>Campers of your dreams</h1>
       <p>You can find everything you want in our catalog</p>
-      <Button onClick={() => {}}>View Now</Button>
+      <Button asNavLink to={routes.catalog()} className={styles.button} onClick={startTransition}>
+        View Now
+      </Button>
     </div>
   );
 };
